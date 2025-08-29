3872d47859273fdacb7bae070f11a5e64e7aaf9d
Rodrigo Junior <rodrigotavaresfranco@gmail.com>
2025-08-29 16:31:14 -0300

    feat/create-module-user

---
+++ src/modules/users/repositories/port/user.repository.port.ts
@@ -0,0 +1,25 @@
+import { UpdateUserDto } from '../../dto/update-user.dto'
+import { CreateUserDto } from '../../dto/create-user.dto'
+import { UserResponseDto } from '../../dto/user-response.dto'
+import { UserResponsePasswordDto } from '../../dto/user-response-password.dto'
+
+export abstract class UserRepositoryPort {
+  abstract findByEmail(email: string): Promise<UserResponseDto | null>
+
+  abstract findByEmailAndPassword(
+    email: string,
+  ): Promise<UserResponsePasswordDto | null>
+
+  abstract findById(id: number): Promise<UserResponseDto | null>
+
+  abstract create(createUserDto: CreateUserDto): Promise<void>
+
+  abstract update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseDto>
+
+  abstract delete(id: number): Promise<void>
+
+  abstract findAll(): Promise<UserResponseDto[]>
+}
---
+++ src/modules/users/repositories/user.repository.ts
@@ -0,0 +1,111 @@
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { User } from '../entities/user.entity'
+import { UserRepositoryPort } from './port/user.repository.port'
+import { UpdateUserDto } from '../dto/update-user.dto'
+import { CreateUserDto } from '../dto/create-user.dto'
+import { UserResponseDto } from '../dto/user-response.dto'
+import { CustomException } from '@/common/exceptions/customException'
+import { ErrorMessages } from '@/common/constants/errorMessages'
+import { UserResponsePasswordDto } from '../dto/user-response-password.dto'
+
+@Injectable()
+export class UserRepository extends UserRepositoryPort {
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: Repository<User>,
+  ) {
+    super()
+  }
+
+  async findByEmail(email: string): Promise<UserResponseDto | null> {
+    const user = await this.repository.findOne({ where: { email } })
+    if (!user) return null
+
+    const dto = new UserResponseDto()
+    dto.id = user.id
+    dto.name = user.name
+    dto.email = user.email
+    dto.createdAt = user.createdAt
+    dto.updatedAt = user.updatedAt
+    return dto
+  }
+
+  async findByEmailAndPassword(
+    email: string,
+  ): Promise<UserResponsePasswordDto | null> {
+    const user = await this.repository.findOne({ where: { email } })
+    if (!user) return null
+
+    const dto = new UserResponsePasswordDto()
+    dto.id = user.id
+    dto.name = user.name
+    dto.email = user.email
+    dto.password = user.password
+    dto.createdAt = user.createdAt
+    dto.updatedAt = user.updatedAt
+    return dto
+  }
+
+  async findById(id: number): Promise<UserResponseDto | null> {
+    const user = await this.repository.findOne({ where: { id } })
+    if (!user) return null
+
+    const dto = new UserResponseDto()
+    dto.id = user.id
+    dto.name = user.name
+    dto.email = user.email
+    dto.createdAt = user.createdAt
+    dto.updatedAt = user.updatedAt
+    return dto
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<void> {
+    const user = this.repository.create(createUserDto)
+    await this.repository.save(user)
+  }
+
+  async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseDto> {
+    await this.repository.update(id, updateUserDto)
+    const updatedUser = await this.repository.findOne({ where: { id } })
+
+    if (!updatedUser) {
+      throw new CustomException(ErrorMessages.USER.NOT_FOUND(id))
+    }
+
+    const dto = new UserResponseDto()
+    dto.id = updatedUser.id
+    dto.name = updatedUser.name
+    dto.email = updatedUser.email
+    dto.createdAt = updatedUser.createdAt
+    dto.updatedAt = updatedUser.updatedAt
+    return dto
+  }
+
+  async delete(id: number): Promise<void> {
+    const user = await this.repository.findOne({ where: { id } })
+
+    if (!user) {
+      throw new CustomException(ErrorMessages.USER.NOT_FOUND(id))
+    }
+
+    await this.repository.remove(user)
+  }
+
+  async findAll(): Promise<UserResponseDto[]> {
+    const users = await this.repository.find()
+    return users.map((user) => {
+      const dto = new UserResponseDto()
+      dto.id = user.id
+      dto.name = user.name
+      dto.email = user.email
+      dto.createdAt = user.createdAt
+      dto.updatedAt = user.updatedAt
+      return dto
+    })
+  }
+}
