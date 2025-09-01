ec231aefef81693eb6332f9e69e6804cd11a02c8
Rodrigo Junior <rodrigotavaresfranco@gmail.com>
2025-09-01 11:39:54 -0300

    feat/auth-login-user

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,21 +1,54 @@
-import { Controller, Post, Body, HttpStatus } from '@nestjs/common'
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common'
 import {
   ApiTags,
   ApiOperation,
   ApiResponse,
+  ApiBody,
   ApiBearerAuth,
+  ApiUnauthorizedResponse,
   ApiBadRequestResponse,
 } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
+import { LoginDto } from './dtos/login.dto'
 import { RegisterDto } from './dtos/register.dto'
 
 import { Public } from '../../common/decorators/public.decorator'
+import { AuthLoginResponseDto } from './dtos/auth-login-response.dto'
 
 @ApiTags('Autenticação')
 @ApiBearerAuth('Bearer')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Public()
+  @ApiOperation({
+    summary: 'Fazer login no sistema',
+    description:
+      'Autentica um usuário no sistema e retorna um token JWT válido',
+  })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Login realizado com sucesso',
+    type: AuthLoginResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Credenciais inválidas',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'number', example: 401 },
+        message: { type: 'string', example: 'Credenciais inválidas' },
+        error: { type: 'string', example: 'Unauthorized' },
+      },
+    },
+  })
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginDto: LoginDto): Promise<AuthLoginResponseDto> {
+    return this.authService.login(loginDto)
+  }
 
   @ApiOperation({
     summary: 'Registrar novo usuário',
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,52 @@
 import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import * as bcrypt from 'bcryptjs'
 import { RegisterDto } from './dtos/register.dto'
+import { AuthUser } from './interfaces/auth-response.interface'
 import { UserRepositoryPort } from '../users/repositories/port/user.repository.port'
 import { CustomException } from '@/common/exceptions/customException'
 import { ErrorMessages } from '@/common/constants/errorMessages'
+import { LoginDto } from './dtos/login.dto'
+import { AuthLoginResponseDto } from './dtos/auth-login-response.dto'
+import { JwtPayload } from '@/common/interfaces/jwt-payload.interface'
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepositoryPort) {}
+  constructor(
+    private userRepository: UserRepositoryPort,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<AuthUser> {
+    const user = await this.userRepository.findByEmailAndPassword(email)
+
+    if (!user) {
+      throw new CustomException(ErrorMessages.USER.INVALID_CREDENTIALS())
+    }
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      }
+    }
+
+    throw new CustomException(ErrorMessages.USER.INVALID_CREDENTIALS())
+  }
+
+  async login(data: LoginDto): Promise<AuthLoginResponseDto> {
+    const user = await this.validateUser(data.email, data.password)
+
+    const payload: JwtPayload = {
+      sub: user.id,
+      email: user.email,
+    }
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    }
+  }
 
   async register(registerDto: RegisterDto): Promise<void> {
     const existingUser = await this.userRepository.findByEmail(
