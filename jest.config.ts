6e85bdcd437549a34c58fbd56b26f97f71d9ff1f
Rodrigo Junior <rodrigotavaresfranco@gmail.com>
2025-09-02 14:45:08 -0300

    feat/config-tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,10 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '.',
+  verbose: true,
+  clearMocks: true,
+  restoreMocks: true,
+  resetMocks: true,
 
   moduleFileExtensions: ['ts', 'js', 'json'],
 
@@ -13,13 +17,40 @@ const config: Config = {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
 
-  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  collectCoverageFrom: [
+    'src/**/*.(t|j)s',
+    '!src/**/*.module.ts',
+    '!src/**/*.dto.ts',
+    '!src/**/*.entity.ts',
+    '!src/**/*.interface.ts',
+    '!src/**/*.enum.ts',
+    '!src/**/*.repository.ts',
+    '!src/main.ts',
+    '!src/config/database/data-source.ts',
+    '!src/migrations/**',
+    '!src/seeds/**',
+    '!src/common/constants/**',
+    '!src/common/decorators/**',
+    '!src/common/exceptions/**',
+    '!src/common/service/**',
+    '!src/common/guards/jwt-auth.guard.ts',
+  ],
 
   coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['text', 'lcov', 'html', 'json'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 
   coveragePathIgnorePatterns: [
     '/node_modules/',
     '/dist/',
+    '/test/',
     '.module.ts$',
     '.dto.ts$',
     '.entity.ts$',
@@ -27,7 +58,7 @@ const config: Config = {
     '.interface.ts$',
     '.enum.ts$',
     'index.ts$',
-    'repository.ts$',
+    '.repository.ts$',
     'src/config/',
     'src/migrations/',
     'src/seeds/',
@@ -41,6 +72,17 @@ const config: Config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+
+  testTimeout: 10000,
+
+  // Configurações para testes de integração
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/test/e2e/'],
+
+  // Configurações para melhor performance
+  maxWorkers: '50%',
+  maxConcurrency: 1,
 }
 
 export default config
